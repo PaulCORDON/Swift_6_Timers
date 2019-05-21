ca810041ac177476b91ffac31b515b09b1076e57
Paul <paul.cordon@laposte.net>
2019-05-21 10:29:37 +0200

    init

--- Timers/ViewController.swift
+++ Timers/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer = Timer()
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+         counter = 10
+        timerLabel.text = String(counter)
+        /*timeInterval : 1 -> la fonction sera lancer tt les secondes*/
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerFunction), userInfo: nil, repeats: true)
     }
-
+    
+    @objc func timerFunction (){
+        //tous ce qui est dans cette fonction sera executer pdt que le timer tourne
+        print("timer running")
+        counter = counter-1
+        timerLabel.text = String(counter)
+       
+        
+        if counter == 0 {
+            timer.invalidate()
+            timerLabel.text = "Time is done!"
+        }
+    }
 
 }
 
